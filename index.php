fe7030d1b244a77ebacf34ec020018b200dc8734
nicolas <nicolas.gpteles@gmail.com>
2024-11-21 10:44:52 -0300

    Tela principal do site

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div class="container">
+        <nav>
+            <script src="https://kit.fontawesome.com/ea4c1412eb.js"crossorigin="anonymous"></script>
+            <div class="navbar">
+                <h1 class="logo">MyPHP</h1>
+                <div class="linha1"></div>
+                <h1 class="site">Sites</h1>
+                <div class="linha2"></div>
+                <div class="linha3"></div>
+                    <div class="search-container">
+                        <input type="text" placeholder="Procurar..." />
+                        <span class="search-icon">
+                            <i class="fa-solid fa-magnifying-glass" style="color: #8c52ff"></i>
+                        </span>
+                    </div>
+                <a href="#" class="login">Login</a>
+            </div>
+        </nav>
+    </div>
+</body>
+</html>
