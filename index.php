87088ca08e29dd2179380a13a9ca5baf80d6d40c
nicolas <nicolas.gpteles@gmail.com>
2024-11-21 13:05:11 -0300

    Atualização da tela principal

--- index.php
+++ index.php
@@ -5,26 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="css/index.css">
+    <script src="https://kit.fontawesome.com/ea4c1412eb.js"crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="container">
-        <nav>
-            <script src="https://kit.fontawesome.com/ea4c1412eb.js"crossorigin="anonymous"></script>
-            <div class="navbar">
-                <h1 class="logo">MyPHP</h1>
-                <div class="linha1"></div>
-                <h1 class="site">Sites</h1>
-                <div class="linha2"></div>
-                <div class="linha3"></div>
-                    <div class="search-container">
-                        <input type="text" placeholder="Procurar..." />
-                        <span class="search-icon">
-                            <i class="fa-solid fa-magnifying-glass" style="color: #8c52ff"></i>
-                        </span>
-                    </div>
-                <a href="#" class="login">Login</a>
-            </div>
+        <nav class="navbar">
+            <h1 class="logo">MyPHP</h1>
+            <div class="linha1"></div>
+            <h1 class="site">Sites</h1>
+            <div class="linha2"></div>
+            <div class="linha3"></div>
+                <div class="search-container">
+                    <input type="text" placeholder="Procurar..." />
+                    <span class="search-icon">
+                        <i class="fa-solid fa-magnifying-glass" style="color: #8c52ff"></i>
+                    </span>
+               </div>
+                <div class="login">
+                    <a href="#" class="login">Login</a>
+                    <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                </div>
         </nav>
+        <button class="iconCriarProjeto">
+            <i class="fa-solid fa-plus plusIcon" aria-hidden="true"></i>
+            <span class="sr-only">Criar Projeto</span>
+        </button>
     </div>
 </body>
 </html>
